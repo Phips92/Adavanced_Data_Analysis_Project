52dc4de03e6f79b83ca3eaaae5f77a07da220b90
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2024-09-19 09:48:04 +0200

    added function clean_text to remove special characters and extra spaces from text

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,4 @@
+import re
 from nltk.corpus import stopwords as nltk_stopwords
 
 class DataPreprocessor:
@@ -5,8 +6,22 @@ class DataPreprocessor:
 
     def __init__(self, stopwords=None):
 
+        #remove stopwords
         self.stopwords = stopwords if stopwords is not None else set(nltk_stopwords.words("english"))
            
 
 
+    def clean_text(self, text):
+
+        #delete everything except []
+        text = re.sub(r"[^a-zA-Z\s]", "", text)
+        text = text.lower()
+        
+        #remove multiple white spaces
+        text = re.sub(r"\s+", " ", text)
+
+        #Remove extra spaces
+        text = text.strip()
+        
+        return text
 
--- test_data_preprocessing.py
+++ test_data_preprocessing.py
@@ -12,3 +12,38 @@ def test_default_stopwords():
 
     processor = DataPreprocessor()
     assert processor.stopwords == set(nltk_stopwords.words("english"))
+
+
+preprocessor = DataPreprocessor()
+
+def test_clean_text():
+    #testcase1: only english letters
+    input_text = "Only Letters"
+    expected_output = "only letters"
+    assert preprocessor.clean_text(input_text) == expected_output
+
+    #testcase2: + numbers and spezial caracter
+    input_text = "6Letters!"
+    expected_output = "letters"
+    assert preprocessor.clean_text(input_text) == expected_output
+
+    #testcase3: multiple spezial characters
+    input_text = "Le3Tt3er2s & @§$%?"
+    expected_output = "letters"
+    assert preprocessor.clean_text(input_text) == expected_output
+
+    #testcase4: only spezial characters
+    input_text = "@!§$%&/()=?"
+    expected_output = ""
+    assert preprocessor.clean_text(input_text) == expected_output
+
+    #testcase5: empty string
+    input_text = ""
+    expected_output = ""
+    assert preprocessor.clean_text(input_text) == expected_output
+
+    #testcase6: extra whitespaces
+    input_text = "   l   e   t t   e  r   s    "
+    expected_output = "l e t t e r s"
+    assert preprocessor.clean_text(input_text) == expected_output
+
