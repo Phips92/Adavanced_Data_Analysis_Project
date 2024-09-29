3eb42ffda8f416454b1ae105417dddf205cec5a3
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2024-09-29 10:50:24 +0200

    Added lsa using BoW and TF-IDF into workflow

--- Use.py
+++ Use.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from data_preprocessing import DataPreprocessor 
-
+from feature_extraction import FeatureExtractor
 
 preprocessor = DataPreprocessor()
+featurizer = FeatureExtractor()
 
 df = pd.read_csv("test_tweets.csv")
 df['text'] = df['text'].apply(preprocessor.clean_text)
@@ -14,11 +15,19 @@ df.to_csv("tokenized_cleaned_test_tweets.csv", index=False)
 
 df = pd.read_csv("tokenized_cleaned_test_tweets.csv")
 corpus = df["text"].values.tolist()
-vectors_tfidf = preprocessor.vectorize(corpus, method="tfidf")
-vectors_bow = preprocessor.vectorize(corpus, method="bow")
+vectors_tfidf = featurizer.vectorize(corpus, method="tfidf")
+vectors_bow = featurizer.vectorize(corpus, method="bow")
 
 #safe vectors
 tfidf_array = vectors_tfidf.toarray()
 bow_array = vectors_bow.toarray()
 pd.DataFrame(tfidf_array).to_csv("TF-IDF_vectors.csv", index=False)
 pd.DataFrame(bow_array).to_csv("BoW_vectors.csv", index=False)
+
+#lsa with tfidf and bow
+lsa_tfidf_vectors = featurizer.use_lsa(vectors_tfidf, n_topics=3)
+print(lsa_tfidf_vectors)
+lsa_bow_vectors = featurizer.use_lsa(vectors_bow, n_topics=3)
+print(lsa_bow_vectors)
+
+
