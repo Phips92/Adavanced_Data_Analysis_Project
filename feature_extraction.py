71f26a90b515ab53e91d8b5735fe5d891a53eaf3
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2024-10-23 13:17:38 +0200

    Modified use_lsa function to reuse SVD method for training and testing set

--- feature_extraction.py
+++ feature_extraction.py
@@ -24,14 +24,16 @@ class FeatureExtractor:
 
 
     #usually performs better with TF-IDF
-    def use_lsa(self, vectors, n_topics=5):
+    def use_lsa(self, vectors, n_topics=6, return_model = False):
 
         svd_model = TruncatedSVD(n_components=n_topics, algorithm="randomized", random_state=42)
         lsa_vectors = svd_model.fit_transform(vectors)
+        if return_model:
+            return svd_model
         return lsa_vectors
 
     #usually performs better with BoW
-    def apply_lda(self, vectors, n_topics=5):
+    def apply_lda(self, vectors, n_topics=6):
 
         lda_model = LatentDirichletAllocation(n_components=n_topics, random_state=42)
         lda_vectors = lda_model.fit_transform(vectors)
