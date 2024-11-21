bc4dac36adedcfe3bfa9c1e905e5e3adda09da51
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2024-11-21 14:09:29 +0100

    Added percentage for better interpretation of the chart

--- visualization_of_datasets.py
+++ visualization_of_datasets.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df_kagel = pd.read_csv("Kagel_dataset_tweets.csv")
+# Load the Kaggle dataset
+df_kaggle = pd.read_csv("Kagel_dataset_tweets.csv")
 
-class_counts = df_kagel["Label"].value_counts()
+# Count the number of samples for each label
+class_counts = df_kaggle["Label"].value_counts()
 class_labels = ["Sadness (0)", "Joy (1)", "Love (2)", "Anger (3)", "Fear (4)", "Surprise (5)"]
 
+
+# Plot a pie chart for class distribution
 plt.figure(figsize=(8, 8))
-plt.pie(class_counts, labels=class_labels)
-plt.title("Class Distribution Kagel Dataset")
+plt.pie(class_counts, labels=class_labels, autopct='%1.1f%%', startangle=90)  # Pie chart with percentage labels
+plt.title("Class Distribution Kaggle Dataset")
 plt.show()
 
 
